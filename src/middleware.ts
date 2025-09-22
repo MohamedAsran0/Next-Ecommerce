f0aee8f85640334a08472dd2b2dd98d8945a560a
Mohamed Asran <78270356+MohamedAsran0@users.noreply.github.com>
2025-09-22 18:47:38 +0300

    remove logs

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,20 +3,14 @@ import { NextRequest, NextResponse } from "next/server";
 
 export default async function middleware(req: NextRequest) {
     
-    console.log('entered middleware');
     // const jwt = await getToken({req});
     const jwt = await getToken({ req, secret: process.env.NEXTAUTH_SECRET,cookieName: process.env.COOKIE_NAME });
-
-    console.log('got token',jwt);
 
     if(jwt) {
         console.log('token true entering next page');
 
         return NextResponse.next();
     }
-
-
-    console.log('token false redirecting');
 
     // return NextResponse.redirect(`${process.env.MY_DOMAIN}`)
     return NextResponse.redirect(new URL("/", req.url));

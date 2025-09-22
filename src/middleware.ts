8525069fd705b7b8bb96f4b4b7d6884d94be0014
Mohamed Asran <78270356+MohamedAsran0@users.noreply.github.com>
2025-09-22 18:25:25 +0300

    soem changes to middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,15 +1,30 @@
-import { getToken } from "next-auth/jwt";
+import { getServerSession } from "next-auth";
+// import { getToken } from "next-auth/jwt";
 import { NextRequest, NextResponse } from "next/server";
+import { authOptions } from "./app/api/auth/[...nextauth]/authOptions";
 
 export default async function middleware(req: NextRequest) {
- 
-    // const jwt = await getToken({req});
-    const jwt = await getToken({ req, secret: process.env.NEXTAUTH_SECRET,cookieName: process.env.COOKIE_NAME });
 
-    if(jwt) {
-        
+    // const jwt = await getToken({req});
+    // const jwt = await getToken({ req, secret: process.env.NEXTAUTH_SECRET,cookieName: process.env.COOKIE_NAME });
+    console.log('entered middleware');
+
+    const session = await getServerSession(authOptions);
+    console.log('got session');
+
+    // if(jwt) {
+
+    //     return NextResponse.next();
+    // }
+
+    if (session) {
+        console.log('session true entering next page');
+
         return NextResponse.next();
+
     }
+
+    console.log('session false redirecting');
 
     // return NextResponse.redirect(`${process.env.MY_DOMAIN}`)
     return NextResponse.redirect(new URL("/", req.url));

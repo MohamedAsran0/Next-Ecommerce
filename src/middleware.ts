c80acee52d6f150f87237b6319d37deab6df43ff
Mohamed Asran <78270356+MohamedAsran0@users.noreply.github.com>
2025-09-22 17:32:20 +0300

    update middleware file

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,19 +3,22 @@ import { NextRequest, NextResponse } from "next/server";
 
 export default async function middleware(req: NextRequest) {
  
-    const jwt = await getToken({req});
+    // const jwt = await getToken({req});
+    const jwt = await getToken({ req, secret: process.env.NEXTAUTH_SECRET });
 
     if(jwt) {
         
         return NextResponse.next();
     }
 
-    return NextResponse.redirect(`${process.env.MY_DOMAIN}`)
+    // return NextResponse.redirect(`${process.env.MY_DOMAIN}`)
+    return NextResponse.redirect(new URL("/", req.url));
 
 }
 
 
 export const config = {
-    matcher: ['/cart:path*', '/profile:path*', '/allorders', '/wishlist']
+    // matcher: ['/cart:path*', '/profile:path*', '/allorders', '/wishlist']
+    matcher: ['/cart/:path*', '/profile/:path*', '/allorders', '/wishlist']
 };
 
